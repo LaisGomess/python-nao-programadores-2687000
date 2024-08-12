d3e2f86345af87a60c46c8e8c54e05125fa9790f
LaisGomess <laahgoomes2002@gmail.com>
2024-08-12 20:39:26 +0000

    variaveis_Python

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -1,11 +1,7 @@
-# Convenções para nomear as variáveis:
-# - Iniciar sempre com letra minúscula;
-# - O nome pode ser composto por letras, algarismos e underlines;
-# - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
-
-# Declare variáveis do tipo string
-
-# Declare variáveis de cada tipo numérico
-
-
-# Declare uma variável do tipo booleano
+#python
+nome = 'variaveis python'
+String = "administradora"
+numero_inteiro = 1
+float = 1.800
+verdadeiro = True
+falso = False
